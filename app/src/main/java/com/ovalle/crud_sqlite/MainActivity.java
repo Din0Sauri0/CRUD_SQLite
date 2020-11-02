a1b5f2e040813d235b49db60a4df08c8f58342e5
Gustavo <gustavo.ovalle.zambrano@gmail.com>
2020-11-02 13:16:26 -0300

    CREATE SELECT

---
+++ app/src/main/java/com/ovalle/crud_sqlite/MainActivity.java
@@ -0,0 +1,100 @@
+package com.ovalle.crud_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.ovalle.crud_sqlite.baseDatos.Utilidades;
+import com.ovalle.crud_sqlite.baseDatos.conexionSQLiteHelper;
+import com.ovalle.crud_sqlite.baseDatos.entidades.Cliente;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    //widget
+    private EditText txtId, txtNombre, txtUsername, txtPassword;
+    private Button btnCrear, btnActualizar, btnEliminar;
+    private ListView ListViewClientes;
+    //manipulacion de BD
+    conexionSQLiteHelper conn = new conexionSQLiteHelper(MainActivity.this, null, null, 1);
+    ArrayList<Cliente> arrayClientes;
+    Cliente cliente;
+    ArrayAdapter<Cliente> adaptadorListView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //UI Referencias
+        txtId = findViewById(R.id.txtId);
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPassword = findViewById(R.id.txtPassword);
+        btnCrear = findViewById(R.id.btnCrear);
+        btnActualizar = findViewById(R.id.btnActualizar);
+        btnEliminar = findViewById(R.id.btnEliminar);
+        ListViewClientes = findViewById(R.id.ListViewClientes);
+        // Started Object
+        arrayClientes = new ArrayList<>();
+        adaptadorListView = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrayClientes);
+        obtenerClientes();
+
+        btnCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //capturar datos escritos por el usuario
+                int id = Integer.parseInt(txtId.getText().toString());
+                String nombre = txtNombre.getText().toString();
+                String userName = txtUsername.getText().toString();
+                String password = txtPassword.getText().toString();
+                //enviar datos al metodo insertarCliente()
+                insertarCliente(id, nombre, userName, password);
+
+            }
+        });
+
+    }
+
+    public void insertarCliente(int id, String nombre, String userName, String password){
+        SQLiteDatabase database = conn.getWritableDatabase();
+        //Crear un Contenedro de valores e insertar
+        ContentValues values = new ContentValues();
+        values.put(Utilidades.CAMPO_ID, id);
+        values.put(Utilidades.CAMPO_NOMBRE, nombre);
+        values.put(Utilidades.CAMPO_USERNAME, userName);
+        values.put(Utilidades.CAMPO_PASSWORD, password);
+
+        database.insert(Utilidades.NOMBRE_TABLE, Utilidades.CAMPO_ID, values);
+        Toast.makeText(MainActivity.this, "Cliente guardado", Toast.LENGTH_LONG).show();
+        database.close();
+        obtenerClientes();
+    }
+
+    public void obtenerClientes(){
+        adaptadorListView.clear();
+        SQLiteDatabase database = conn.getReadableDatabase();
+        Cursor cursor = database.rawQuery("SELECT * FROM "+Utilidades.NOMBRE_TABLE,null);
+        while(cursor.moveToNext()){
+            cliente = new Cliente(
+                    cursor.getInt(0),//id
+                    cursor.getString(1),//nombre
+                    cursor.getString(2),//username
+                    cursor.getString(3)//password
+            );
+            arrayClientes.add(cliente);
+        }
+        adaptadorListView = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrayClientes);
+        ListViewClientes.setAdapter(adaptadorListView);
+        cursor.close();
+        conn.close();
+    }
+}
---
+++ app/src/main/java/com/ovalle/crud_sqlite/baseDatos/conexionSQLiteHelper.java
@@ -0,0 +1,26 @@
+package com.ovalle.crud_sqlite.baseDatos;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class conexionSQLiteHelper extends SQLiteOpenHelper {
+
+
+    public conexionSQLiteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, "bd_clientes", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(Utilidades.CREAR_TABLA_CLEINTE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS clientes");
+        onCreate(sqLiteDatabase);
+    }
+}
